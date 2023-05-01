45aca3937255709abe352378e203eb8137a6afb9
raymondkneipp <raymond.f.kneipp@gmail.com>
2023-05-01 15:26:48 -0700

    finish index and create endpoints

--- app/controllers/trees_controller.rb
+++ app/controllers/trees_controller.rb
@@ -1,2 +1,16 @@
 class TreesController < ApplicationController
+  def index
+    trees = Tree.all
+    render json: trees
+  end
+
+  def create
+    tree = Tree.create(tree_params)
+    render json: tree
+  end
+
+  private
+  def tree_params
+    params.require(:tree).permit(:name, :age, :enjoys, :image)
+  end
 end
--- spec/requests/trees_spec.rb
+++ spec/requests/trees_spec.rb
@@ -2,6 +2,41 @@ require 'rails_helper'
 
 RSpec.describe "Trees", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+
+    it "should get a list of trees" do
+      Tree.create(
+        name: 'Maple',
+        age: 150,
+        enjoys: 'Making syrup',
+        image: 'https://cdn.shopify.com/s/files/1/0059/8835/2052/products/Sugar-Maple_FGT_600x600_6ad4956a-ec6e-4ab2-a66a-1a1245471195_grande.jpg?v=1612444329'
+      )
+
+      get '/trees'
+
+      trees = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(trees.length).to eq 1
+    end
+  end
+
+  describe "POST /create" do
+    it "should create a new tree" do
+      tree_params = {
+        tree: {
+          name: 'Maple',
+          age: 150,
+          enjoys: 'Making syrup',
+          image: 'https://cdn.shopify.com/s/files/1/0059/8835/2052/products/Sugar-Maple_FGT_600x600_6ad4956a-ec6e-4ab2-a66a-1a1245471195_grande.jpg?v=1612444329'
+        }
+      }
+
+      post '/trees', params: tree_params
+
+      expect(response).to have_http_status(200)
+
+      tree = Tree.first
+
+      expect(tree.name).to eq 'Maple'
+    end
   end
 end
